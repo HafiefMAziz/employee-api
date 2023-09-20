65354f562b05407e045176fa3311437499214c62
Hafief Maulana Aziz <Hafief.Maulana.Aziz@hashmicro.com>
2023-09-20 10:39:34 +0700

    selesai employee

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,7 +3,7 @@ const EmployeeController = require('../controllers/EmployeeController.js')
 
 
 employeeRoute.get('/', EmployeeController.show)
-// employeeRoute.get('/:id', EmployeeController.show)
+employeeRoute.get('/:id', EmployeeController.show)
 employeeRoute.post('/create', EmployeeController.create)
 employeeRoute.get('/delete/:id', EmployeeController.delete)
 employeeRoute.post('/update/:id', EmployeeController.update)
